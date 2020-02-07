2e87cc45483ba6278a866165db430f42edef79b3
osuarez <vegetali@gmail.com>
2020-02-07 17:43:16 +0100

    feat(main): added code to fix the issue with the partition code and also to use the DF generated in memory for testing.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from sklearn.preprocessing import LabelEncoder
 # GLOBAL VARIABLES
 ##################
 # List of nodes to test
-n = [2, 4]#[2, 4, 7, 11]
+n = [2, 4, 7, 11]
 # Number of samples per node
 m = 200
 # Number of executions per dataset
@@ -186,9 +186,6 @@ for n_exec in range(0, nexec):
         m
     )
     balanced_dataset.to_csv('first_balanced_dataset.csv', index=False)
-    # UNBALANCED DATASET
-    # TODO
-
 
     for number_of_nodes in n:
         print(f'Preparing experiment for {number_of_nodes} nodes...\n')
@@ -236,28 +233,35 @@ for n_exec in range(0, nexec):
 
             start_balanced_enegery_distance_computing = time.time()
             # Iterate over each node of the list
+            node_counter = 0
             for node in l_df_balanced_partitioned_nodes:
                 # Iterate over all the rows in the DataFrame of the node
                 print('Computing Energy Statistic for balanced nodes...')
                 for index, row in node.iterrows():
                     e = energy_statistic_b(row, df_original_test)
                     #print(e)
-                    df_dist_balanced_list_all_nodes = df_dist_balanced_list_all_nodes.append(pd.DataFrame([[0, index, e]], columns=col_names), ignore_index=True)
+                    df_dist_balanced_list_all_nodes = df_dist_balanced_list_all_nodes.append(pd.DataFrame([[node_counter, index, e]], columns=col_names), ignore_index=True)
+                node_counter += 1
                 
             # Sort the list using the Energy Statistic distance and taking into account all the nodes
             df_dist_balanced_list_all_nodes = df_dist_balanced_list_all_nodes.sort_values(by=['Energy_statistic'], ascending=True)
 
-            df_dist_balanced_list_all_nodes.to_csv('lista_nodos.csv')
+            df_dist_balanced_list_all_nodes.to_csv('lista_nodos' + str(number_of_nodes) + '.csv')
 
             print(df_dist_balanced_list_all_nodes.head())
             # Take only the m better observations order by Energy Statistic
-            df_training_balanced_all_nodes = df_dist_balanced_list_all_nodes.iloc[:m,:]
+            df_training_balanced_all_nodes = df_dist_balanced_list_all_nodes.iloc[:math.floor(m/number_of_nodes),:]
+            df_training_balanced_all_nodes.to_csv('lista_nodo_best_es_training_balanced.csv')
 
             df_training_distributed_balanced_final_node = pd.DataFrame([])
-            for i in range(0, number_of_nodes):
-                df_training_distributed_balanced_final_node = df_training_distributed_balanced_final_node.append(l_df_balanced_partitioned_nodes[i].iloc[df_training_balanced_all_nodes[df_training_balanced_all_nodes['Node'] == i].Index], ignore_index=True)
+            # for i in range(0, number_of_nodes):
+            #     df_training_distributed_balanced_final_node = df_training_distributed_balanced_final_node.append(l_df_balanced_partitioned_nodes[i].iloc[df_training_balanced_all_nodes[df_training_balanced_all_nodes['Node'] == i].Index], ignore_index=True)
+            node_counter = 0
+            for node in l_df_balanced_partitioned_nodes:
+                df_training_distributed_balanced_final_node = df_training_distributed_balanced_final_node.append(node.loc[df_training_balanced_all_nodes[df_training_balanced_all_nodes['Node'] == node_counter].Index], ignore_index=True)
+                node_counter += 1
 
-            df_training_distributed_balanced_final_node.to_csv('training_distributed_balanced_final.csv')
+            df_training_distributed_balanced_final_node.to_csv('training_distributed_balanced_final' + str(number_of_nodes) + '.csv')
             end_balanced_energy_distance_computing = time.time()
             print(f'Elapsed time in computing energy distance in balanced partitions: {end_balanced_energy_distance_computing - start_balanced_enegery_distance_computing}')
 
@@ -289,7 +293,7 @@ for n_exec in range(0, nexec):
             # Take only the (m / number_of_nodes) better observations order by Energy Statistic
             df_training_unbalanced_all_nodes = df_dist_unbalanced_list_all_nodes.iloc[:math.floor(m/number_of_nodes),:]
 
-            df_training_unbalanced_all_nodes.to_csv('lista_nodo_best_es_training_unbalanced.csv', index=False)
+            df_training_unbalanced_all_nodes.to_csv('lista_nodo_best_es_training_unbalanced' + str(number_of_nodes) + '.csv', index=False)
 
             df_training_distributed_unbalanced_final_node = pd.DataFrame([])
 
@@ -304,7 +308,7 @@ for n_exec in range(0, nexec):
             end_unbalanced_energy_distance_computing = time.time()
             print(f'Elapsed time in computing energy distance in unbalanced partitions: {end_unbalanced_energy_distance_computing - start_unbalanced_enegery_distance_computing}')
 
-            df_training_distributed_unbalanced_final_node.to_csv('lista_nodo_training_final.csv', index=False)
+            df_training_distributed_unbalanced_final_node.to_csv('lista_nodo_training_final' + str(number_of_nodes) + '.csv', index=False)
 
             # # TESTING
             # node_counter = 0
@@ -409,17 +413,20 @@ for n_exec in range(0, nexec):
         # TODO
         # esto hay que cambiarlo para utilizar el dataset guardado en
         # el dataframe al principio
-        X_train_centralized_balanced = pd.read_csv(
-            'sampled_centralized_balanced.csv', 
-            sep=',',
-            header=0
-        ).iloc[:,:-1]
+        # X_train_centralized_balanced = pd.read_csv(
+        #     'sampled_centralized_balanced.csv', 
+        #     sep=',',
+        #     header=0
+        # ).iloc[:,:-1]
         
-        y_train_centralized_balanced = pd.read_csv(
-            'sampled_centralized_balanced.csv', 
-            sep=',', 
-            header=0
-        ).iloc[:, -1]
+        # y_train_centralized_balanced = pd.read_csv(
+        #     'sampled_centralized_balanced.csv', 
+        #     sep=',', 
+        #     header=0
+        # ).iloc[:, -1]
+
+        X_train_centralized_balanced = balanced_dataset.iloc[:, :-1]
+        y_train_centralized_balanced = balanced_dataset.iloc[:, -1]
 
         for classifier, name in zip(list_classifiers,list_classifiers_names):
             print(f'\nThe classifier: {name}\n')
@@ -429,9 +436,9 @@ for n_exec in range(0, nexec):
             print(f'Elapsed time in training for balanced centralized: {end - start}')
 
             y_pred = classifier.predict(X_test.values)
-            print(f'The recall for {number_of_nodes} nodes in balanced centralized ' \
+            print(f'The recall in balanced centralized ' \
                 f'is {recall_score(y_test, y_pred, average="macro")}.')
-            print(f'The precision for {number_of_nodes} nodes in balanced centralized ' \
+            print(f'The precision balanced centralized ' \
                 f'is {precision_score(y_test, y_pred, average="macro")}.')
 
             # Append the info to the DataFrame...
