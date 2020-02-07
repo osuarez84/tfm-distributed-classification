3132d9f567fa41290d96a6ffb0f4b8c7e8cb259d
osuarez <vegetali@gmail.com>
2020-02-07 19:03:13 +0100

    feat(main): added code to compute the ranking of distances not in global but in each node, so we then extract the best instances from each node individually.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ n = [2, 4, 7, 11]
 m = 200
 # Number of executions per dataset
 nexec = 1
-is_balanced = True
+is_balanced = False
 
 #########################
 # Setting the classifiers
@@ -205,11 +205,6 @@ for n_exec in range(0, nexec):
             print(f'Number of nodes in the balanced list: {len(l_df_balanced_partitioned_nodes)}')
         else:
             print('Unbalanced partition...')
-            # l_df_unbalanced_partitioned_nodes = unbalanced_dataset_generation(
-            #     df_original_train,
-            #     number_of_nodes,
-            #     m
-            # )
             start_unbalanced_partition = time.time()
             l_df_unbalanced_partitioned_nodes = create_unbalanced_partitions(
                 balanced_dataset,
@@ -229,42 +224,48 @@ for n_exec in range(0, nexec):
         if (is_balanced):
             # Prepare the list with Energy Statistic info
             col_names = ['Node', 'Index', 'Energy_statistic']
-            df_dist_balanced_list_all_nodes = pd.DataFrame([], columns=col_names)
+
+            # Guardamos una lista donde cada elemento es un dataframe con
+            # los valores de distancia,nodo, indice para cada nodo disponible            
+            l_df_total_energy = []
 
             start_balanced_enegery_distance_computing = time.time()
             # Iterate over each node of the list
             node_counter = 0
             for node in l_df_balanced_partitioned_nodes:
+                df_dist_node_tmp = pd.DataFrame([], columns=col_names)
                 # Iterate over all the rows in the DataFrame of the node
                 print('Computing Energy Statistic for balanced nodes...')
                 for index, row in node.iterrows():
                     e = energy_statistic_b(row, df_original_test)
                     #print(e)
-                    df_dist_balanced_list_all_nodes = df_dist_balanced_list_all_nodes.append(pd.DataFrame([[node_counter, index, e]], columns=col_names), ignore_index=True)
+                    df_dist_node_tmp = df_dist_node_tmp.append(pd.DataFrame([[node_counter, index, e]], columns=col_names), ignore_index=True)
+                l_df_total_energy.append(df_dist_node_tmp)
                 node_counter += 1
+
+            # Ordenamos las distancias en cada elemento de la lista (nodo)
+            for i in l_df_total_energy:
+                i.sort_values(by=['Energy_statistic'], ascending=True, inplace=True)
                 
-            # Sort the list using the Energy Statistic distance and taking into account all the nodes
-            df_dist_balanced_list_all_nodes = df_dist_balanced_list_all_nodes.sort_values(by=['Energy_statistic'], ascending=True)
-
-            df_dist_balanced_list_all_nodes.to_csv('lista_nodos' + str(number_of_nodes) + '.csv')
-
-            print(df_dist_balanced_list_all_nodes.head())
-            # Take only the m better observations order by Energy Statistic
-            df_training_balanced_all_nodes = df_dist_balanced_list_all_nodes.iloc[:math.floor(m/number_of_nodes),:]
-            df_training_balanced_all_nodes.to_csv('lista_nodo_best_es_training_balanced.csv')
-
+            # Iteramos por cada nodo de la lista y recogemos las m / numero_de_nodos 
+            # mejores muestras para formar el dataset de training
             df_training_distributed_balanced_final_node = pd.DataFrame([])
-            # for i in range(0, number_of_nodes):
-            #     df_training_distributed_balanced_final_node = df_training_distributed_balanced_final_node.append(l_df_balanced_partitioned_nodes[i].iloc[df_training_balanced_all_nodes[df_training_balanced_all_nodes['Node'] == i].Index], ignore_index=True)
-            node_counter = 0
-            for node in l_df_balanced_partitioned_nodes:
-                df_training_distributed_balanced_final_node = df_training_distributed_balanced_final_node.append(node.loc[df_training_balanced_all_nodes[df_training_balanced_all_nodes['Node'] == node_counter].Index], ignore_index=True)
-                node_counter += 1
 
-            df_training_distributed_balanced_final_node.to_csv('training_distributed_balanced_final' + str(number_of_nodes) + '.csv')
+            #counter = 0
+            for node_energy, node in zip(l_df_total_energy, l_df_balanced_partitioned_nodes):
+                tmp_df_energy = node_energy.iloc[:math.floor(m/(number_of_nodes**2)), :]
+                #tmp_df_energy.to_csv('list_energy_node_' + str(counter) + '.csv' )
+                #node.to_csv('node_instances_' + str(counter) + '.csv')
+                df_training_distributed_balanced_final_node =  df_training_distributed_balanced_final_node.append(node.loc[tmp_df_energy['Index']])
+                #counter += 1
+
+            # print(df_training_distributed_balanced_final_node.shape)
+            # print(df_training_distributed_balanced_final_node.head())
+            # print(df_training_distributed_balanced_final_node.tail())
+
+            #df_training_distributed_balanced_final_node.to_csv('training_distributed_balanced_final' + str(number_of_nodes) + '.csv')
             end_balanced_energy_distance_computing = time.time()
             print(f'Elapsed time in computing energy distance in balanced partitions: {end_balanced_energy_distance_computing - start_balanced_enegery_distance_computing}')
-
         #################
         # CASE UNBALANCED
         #################
@@ -272,50 +273,43 @@ for n_exec in range(0, nexec):
             # Prepare the list with Energy Statistic info
             col_names = ['Node', 'Index', 'Energy_statistic']
             df_dist_unbalanced_list_all_nodes = pd.DataFrame([], columns=col_names)
+            
+            l_df_total_energy = []
 
             start_unbalanced_enegery_distance_computing = time.time()
             # Iteramos sobre cada nodo para computar la distancia de cada una
             # de sus instancias
             node_counter = 0
             for node in l_df_unbalanced_partitioned_nodes:
+                df_dist_node_tmp = pd.DataFrame([], columns=col_names)
                 print('Computing Energy Statistic for unbalanced nodes...')
                 for index, row in node.iterrows():
                     e = energy_statistic_b(row, df_original_test)
                     #print(e)
-                    df_dist_unbalanced_list_all_nodes = df_dist_unbalanced_list_all_nodes.append(pd.DataFrame([[node_counter, index, e]], columns=col_names), ignore_index=True)
+                    df_dist_node_tmp = df_dist_node_tmp.append(pd.DataFrame([[node_counter, index, e]], columns=col_names), ignore_index=True)
+                l_df_total_energy.append(df_dist_node_tmp)
                 node_counter += 1
 
-            # Sort the list using the Energy Statistic distance and taking into account all the nodes
-            df_dist_unbalanced_list_all_nodes = df_dist_unbalanced_list_all_nodes.sort_values(by=['Energy_statistic'], ascending=True)
-            print(df_dist_unbalanced_list_all_nodes.head())
-            df_dist_unbalanced_list_all_nodes.to_csv('lista_nodos_es_unbalanced.csv', index=False)
-            
-            # Take only the (m / number_of_nodes) better observations order by Energy Statistic
-            df_training_unbalanced_all_nodes = df_dist_unbalanced_list_all_nodes.iloc[:math.floor(m/number_of_nodes),:]
+            # Ordenamos las distancias en cada elemento de la lista (nodo)
+            for i in l_df_total_energy:
+                i.sort_values(by=['Energy_statistic'], ascending=True, inplace=True)
 
-            df_training_unbalanced_all_nodes.to_csv('lista_nodo_best_es_training_unbalanced' + str(number_of_nodes) + '.csv', index=False)
-
+            # Iteramos por cada nodo de la lista y recogemos las m / numero_de_nodos 
+            # mejores muestras para formar el dataset de training
             df_training_distributed_unbalanced_final_node = pd.DataFrame([])
 
-            # Recogemos las muestras de cada nodo basandonos en el índice 
-            # y el ranking creado a partir de la distancia distribucional
-            # Esto nos genera nuestro dataset de training => df_training_distributed_unbalanced_final_node
-            node_counter = 0
-            for node in l_df_unbalanced_partitioned_nodes:
-                df_training_distributed_unbalanced_final_node = df_training_distributed_unbalanced_final_node.append(node.loc[df_training_unbalanced_all_nodes[df_training_unbalanced_all_nodes['Node'] == node_counter].Index], ignore_index=True)
-                node_counter += 1
+            #counter = 0
+            for node_energy, node in zip(l_df_total_energy, l_df_unbalanced_partitioned_nodes):
+                tmp_df_energy = node_energy.iloc[:math.floor(m/(number_of_nodes**2)), :]
+                #tmp_df_energy.to_csv('list_energy_node_' + str(counter) + '.csv' )
+                #node.to_csv('node_instances_' + str(counter) + '.csv')
+                df_training_distributed_unbalanced_final_node =  df_training_distributed_unbalanced_final_node.append(node.loc[tmp_df_energy['Index']])
+                #counter += 1
 
             end_unbalanced_energy_distance_computing = time.time()
             print(f'Elapsed time in computing energy distance in unbalanced partitions: {end_unbalanced_energy_distance_computing - start_unbalanced_enegery_distance_computing}')
 
             df_training_distributed_unbalanced_final_node.to_csv('lista_nodo_training_final' + str(number_of_nodes) + '.csv', index=False)
-
-            # # TESTING
-            # node_counter = 0
-            # for node in l_df_unbalanced_partitioned_nodes:
-            #     node.to_csv('node_unbalanced_' + str(node_counter) + '.csv')
-            #     node_counter += 1
-            # sys.exit(1)
 
         #################################
         # PREPARE TEST DATASET
